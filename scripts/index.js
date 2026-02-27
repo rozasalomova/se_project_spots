b7e9c3f70ec7d193621023a9fee2ebb1360c7d12
Roza Hashimjanova <rozasalomova30@gmail.com>
2026-02-26 20:29:13 -0500

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,13 @@ const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+
+console.log("newPostBtn:", newPostBtn);
+console.log("newPostModal:", newPostModal);
+console.log("newPostCloseBtn:", newPostCloseBtn);
 
 editProfileBtn.addEventListener("click", function () {
     editProfileModal.classList.add("modal_is-opened");
